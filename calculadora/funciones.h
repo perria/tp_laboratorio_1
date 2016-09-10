7ef97c41b673539a392ccb2d054b0b2b689babc1
perria <ailenperri.ap@gmail.com>
2016-09-09 21:09:44 -0300

    Calculadora

---
+++ calculadora/funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float sumar(float a, float b);
+float restar(float a, float b);
+float dividir(float a, float b);
+float multiplicar(float a, float b);
+int factorial(int numero);
+int menu(int opcion);
+float numeroU(float numUno);
+float numeroD(float numDos);
+
+
+#endif // FUNCIONES_H_INCLUDED
